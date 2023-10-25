406ee466931a21f4c6ec4ef6f86e8d8e1b880954
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-25 13:21:27 -0400

    got JSON data

---
+++ src/main.js
@@ -0,0 +1,29 @@
+let nasaData;
+
+function init() {
+    // wish I could hide api keys but this is on Github static page (i think there is a way)
+    const API_KEY = `3qTHhoutarfLnx81918TdVY6FpFTFEW5ijlwA0P1`
+    
+    let userStr
+
+    async function getNASAPics() {
+        let url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=3&hd=false&thumbs=false`;
+        const response = await fetch(url);
+        const nasaData = await response.json();
+        console.log(nasaData);
+    
+    
+      }
+    
+    
+    getNASAPics();
+}
+
+
+
+
+// eventListeners
+
+function initEventListeners(){
+    
+}
