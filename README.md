14160e918567b4f8402207107f8f2f1fab4c95b1
Thiem Huynh <thiem.huynh0@gmail.com>
2023-10-25 23:36:47 -0400

    finished event handlers 4 new cards. refresh toast

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# FE-NASA-photo
-frontend project for nasa photo of the day
+
+
+
+Placeholder for image`<img src="https://raw.githubusercontent.com/business-thiem/NextJS13-scott-tut/main/assets/NextJS13-todo-ss1.png" width="1200" height="500">`
+
+# App Overview
+### FE-NASA-photo
+A web app that shows photos published by NASA between X and Y dates.
+
+
+Link to App: 
+
+## App Info:
+
+
+## Tech Used: 
+- NASA API : https://api.nasa.gov/
+- TailwindCSS : CSS Framework (lightweight adaptable)
+- Toastify JS: A lightweight JS notification library. See: https://apvarun.github.io/toastify-js/ 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 */
 
 
-// let nasaData;
+let nasaData = [];
+
 window.addEventListener('DOMContentLoaded', (event) => {
     init();
     console.log('DOM fully loaded and parsed');
@@ -18,51 +19,101 @@ function init() {
     const searchBtn = document.querySelector('#searchBtn')
     const startDateBtn = document.querySelector('#startDate')
     const endDateBtn = document.querySelector('#endDate')
+    const loadingIcon = document.querySelector('#loading-icon')
+
+    const successToast = () => {
+                            Toastify({
+                                text: `Fetch Success!`,
+                                duration: 2000,
+                                gravity: "top",
+                                close: true,
+                                position:"center",
+                                style: {
+                                    background: "linear-gradient(to right, rgb(0, 176, 155), rgb(150, 201, 61))"
+                                }
+                                }).showToast()
+                            }
+
+    const failToast = (error) => {
+                            Toastify({
+                                text: `Error with getting pics ${error.name}`,
+                                duration: 3000,
+                                gravity: "top",
+                                close: true,
+                                position:"center",
+                                style: {
+                                    background: "linear-gradient(to right, rgb(255, 95, 109), rgb(255, 195, 113))"
+                                }
+                                }).showToast()
+                            }
 
     //default date set
     let startDate = ``
     let endDate = ``;
 
-
-
     //function calls
     initEventListeners();
-
-    
 
 
     //functions declarations
     async function getNASAPics() {
+        let response;
+
+        //empty dates
+        if(!startDate || !endDate ){
+            alert('empty dates');
+            startDateBtn.parentElement.classList.toggle("bg-red-500/50")
+            setTimeout(()=>{
+                startDateBtn.parentElement.classList.toggle("bg-red-500/50")
+            }, 3000)
+
+            return;
+        }
+
         let url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&hd=false&thumbs=false&start_date=${startDate}&end_date=${endDate}`;
-        const response = await fetch(url);
-        const nasaData = await response.json();
-        console.log(nasaData);
-    
+
+        loadingIcon.classList.toggle('hidden')
+        
+        try {
+            response = await fetch(url);
+            nasaData = await response.json();
+            successToast();
+        } catch (error){
+            console.error('Error: did not get NASA Pics', error)
+            failToast(error)
+        }
+
+        loadingIcon.classList.toggle('hidden')
         generateCards(nasaData);
+        
       }
 
     // eventListeners
     function initEventListeners(){
-        searchBtn.addEventListener('click', (e) => {
-            console.log('clicked')
 
-            // let url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=7&hd=false&thumbs=false&start_date=${startDate}&endDate=${endDate}`;
-            // console.log(url)
-
+        const cards = document.querySelectorAll(".card") 
+        
+        searchBtn.addEventListener('click', () => {
             getNASAPics();
         })
         
         startDateBtn.addEventListener('input', (e) => {
             startDate = e.target.value 
-            console.log('start date', e.target.value)
         })
 
         endDateBtn.addEventListener('input', (e) => {
             endDate = e.target.value 
-            console.log('end date', e.target.value)
         })
         
+
+        cards.forEach(card => {
+            card.addEventListener('click', (e) =>{
+                console.log('clicked on', e.target.parentElement.id)
+            })
+        })
     }
+
+
 }
 
 // TODO account for num of a photos
@@ -70,11 +121,13 @@ function generateCards(arr){
     
     const appContents = document.querySelector("#app-contents")
 
-    let appContentsHTML = ``
+    //empty out contents
+    appContents.innerHTML = ``;
+    let appContentsHTML = ``;
 
-    arr.forEach((obj) => {
+    arr.forEach((obj, index) => {
         appContentsHTML +=`
-                <div class="card group relative col-span-2 max-h-[900px] rounded-2xl overflow-hidden text-center justify-center items-center cursor-pointer bg-slate-100 shadow-md hover:bg-slate-200 hover:shadow-xl 
+                <div id="${index}" class="card group relative col-span-2 max-h-[900px] rounded-2xl overflow-hidden text-center justify-center items-center cursor-pointer bg-slate-100 shadow-md hover:bg-slate-200 hover:shadow-xl 
                 ">
                     <div class="img-ctn h-96 w-96 block">
                         <img src="${obj.url}" alt="${obj.title}" 
@@ -84,8 +137,8 @@ function generateCards(arr){
                     <div class="curtain absolute inset-0 bg-transparent group-hover:bg-gray-800/80"></div>
             
                     <div class="text-ctn absolute inset-0 flex flex-col text-gray-100 px-9 translate-y-[110%] overflow-hidden group-hover:translate-y-[30%]" >
-                        <h1 class="text-3xl font-bold">Title</h1>
-                        <div class="text-lg p-1">Date: ${obj.title}</div>
+                        <h1 class="text-3xl font-bold">${obj.title}</h1>
+                        <div class="text-lg p-1">Date: ${obj.date}</div>
                         <p class="overflow-hidden">Description: ${obj.explanation}</p>
                     </div>
                 </div>
@@ -94,6 +147,30 @@ function generateCards(arr){
     })
 
     appContents.innerHTML = appContentsHTML
+
+
+    const cards = document.querySelectorAll(".card") 
+    initCardListeners(cards)
+
+}
+
+function initCardListeners(cardElements){
+
+    cardElements.forEach(card => {
+        card.addEventListener('click', (e) => {
+            // console.log('clicked on', e.target.parentElement.id)
+            loadModal(e.target.parentElement.id)
+        })
+    })
+
+    //arr[i].addEventListener{this.id}
+}
+
+
+function loadModal(id){
+    console.log(nasaData[id])
+
+    
 }
 
 
